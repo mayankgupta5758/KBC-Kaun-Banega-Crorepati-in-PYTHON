0519d66e6bb4e2f956389da9c45675f6ed89f09d
Mayank Gupta <mayankg5758@gmail.com>
2023-11-07 17:05:49 -0800

    Add a PYTHON file

---
+++ KBC.py
@@ -0,0 +1,128 @@
+import time
+
+print(" Welcome In KBC!!! ")
+print(" -->See The Input Letter Is Cpital ⚠⚠⚠⚠⚠⚠")
+print(" ")
+x = input(" Do You Want To Play 'YES OR NO': ")
+if x == "YES":
+    print(" ")
+    print(" So Rules Are As Follow's: ")
+    print(" You Have To Give Answer of 5 Question ")
+    print(" The Question Are Based On SHANTAN DHARM")
+    y = input(" Do You Understand 'YES OR NO': ")
+    if y == " YES":
+        print(" ")
+        print(" 1st Question Prize Is 10,000Rs")
+        print(" 2nd Question Prize Is 50,000Rs")
+        print(" 3rd Question Prize Is 1,00,000Rs")
+        print(" 4th Question Prize Is 5,00,000Rs")
+        print(" 5th Question Prize Is 10,00,000Rs")
+    else:
+        print(" ")
+        print(" Dhubara Dhek: ")
+        print(" 5 Question Ke Answer Dena Hai Thume")
+        print(" 1st Question Prize Is 10,000Rs")
+        print(" 2nd Question Prize Is 50,000Rs")
+        print(" 3rd Question Prize Is 1,00,000Rs")
+        print(" 4th Question Prize Is 5,00,000Rs")
+        print(" 5th Question Prize Is 10,00,000Rs")
+    print(" LET'S GET  STARTED")
+    print(" GET READY FOR FIRST QUESTION")
+    print(" \n")
+    print(
+        "-----------------------------------------------------------------------------"
+    )
+    print("                   1. How Many Avtar Of Lord Vinshu Have?")
+    print("                            Your Option Are: ")
+    print(
+        """              A. 10                               B. 11
+              C. 8                                D. 9"""
+    )
+    z = input("                      Enter Your Answer: ")
+    if z == "A":
+        print("                CORRECT ANSWER🎉🎉🎉🎉🎉🎉🎉🎉")
+        print(" \n")
+        print(
+            "-----------------------------------------------------------------------------"
+        )
+        print("                  2. Get Ready For 2nd Question: ")
+        print("                  BARANJ DAL Ki Sthpana Kab Hui Thi?")
+        print("                            Your Option Are: ")
+        print(
+            """              A. 1984                               B. 1986
+              C. 1985                               D. 1987"""
+        )
+        z = input("                      Enter Your Answer: ")
+        if z == "C":
+            print("                CORRECT ANSWER🎉🎉🎉🎉🎉🎉🎉🎉")
+
+            print(" \n")
+            print(
+                "-----------------------------------------------------------------------------"
+            )
+            print("                        Your 3rd Question Is: ")
+            print("                  3. Which Is The Oldest Country In The World?")
+            print("                            Your Option Are: ")
+            print(
+                """              A. CHINA                               B. EJYPT
+              C. JAPAN                               D. INDIA"""
+            )
+            z = input("                      Enter Your Answer: ")
+            if z == "D":
+                print("                CORRECT ANSWER🎉🎉🎉🎉🎉🎉🎉🎉")
+                print(" \n")
+                print(
+                    "-----------------------------------------------------------------------------"
+                )
+                print("                    Get Ready For 4th Question: ")
+                print(
+                    "                                 4. Who Whrote Sri BHAWAT Khata?"
+                )
+                print("                                         Your Option Are: ")
+                print(
+                    """                  A. NARAD MUNI JI                                B. VED VEYAS JI
+                  C. GIRGAS JI                                    D. NONE OF THESE"""
+                )
+                z = input("                      Enter Your Answer: ")
+                if z == "B":
+                    print("                CORRECT ANSWER🎉🎉🎉🎉🎉🎉🎉🎉")
+                    print(" \n")
+                    print(
+                        "-----------------------------------------------------------------------------"
+                    )
+                    print("                    Get Ready For 5th Question: ")
+                    print(
+                        "                             5. How Many Granth Wrote By VED VIVAS JI?"
+                    )
+                    print("                                         Your Option Are: ")
+                    print(
+                        """                    A. 11                                   B. 17
+                    C. 20                                   D. 15"""
+                    )
+                    z = input("                      Enter Your Answer: ")
+                    if z == "B":
+                        print("                CORRECT ANSWER🎉🎉🎉🎉🎉🎉🎉🎉")
+                        print(" \n")
+                        print(
+                            "-----------------------------------------------------------------------------"
+                        )
+                        print(" YOU WON THE KBC AND YOUR WINING AMOUNT IS 10,00,000Rs ONLY 🎉🎉🎉🎉")
+                        print("-----------------------------------------------------------------------------")
+                    else:
+                        print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINING AMOUNT IS 5,00,000Rs O NLY")
+
+                else:
+                    print("  YOU LOST THE KBC 😥😥😥😥  AND YOUR WINING AMOUNT IS 1,00,000Rs O")
+
+            else:
+                print("  YOU LOST THE KBC 😥😥😥😥  AND YOUR WINING AMOUNT IS 50,000Rs O")
+
+        else:
+            print("  YOU LOST THE KBC 😥😥😥😥  AND YOUR WINING AMOUNT IS 10,000Rs ON")
+
+    else:
+        print("                    YOU LOST THE KBC CHALA JA YAHA SE KAMIN 😥😥😥😥 ")
+
+else:
+    print(" ")
+    print(" Chala Ja Yaha Se Kamin Marega Kya 😡😡😡😡😡 ")
